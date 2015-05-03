a698cdc662d5e40873547be57844275b3ab27aa5
likcoras <lck.ogc@gmail.com>
2015-05-02 18:37:58 -0600

    Extremely minor formatting

--- src/main/java/io/github/likcoras/ssbot/backends/BttHandler.java
+++ src/main/java/io/github/likcoras/ssbot/backends/BttHandler.java
@@ -108,6 +108,7 @@ public class BttHandler implements DataHandler {
 		final Elements tableRows =
 			doc.getElementsByTag("table").get(0)
 				.getElementsByTag("td[font-weight]");
+		
 		for (final Element row : tableRows) {
 			
 			final String header = row.text();
