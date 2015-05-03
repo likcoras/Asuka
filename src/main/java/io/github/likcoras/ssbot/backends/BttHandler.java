71b56ec09a823a228aadb7a4c7b7e2791dcdbe7c
likcoras <lck.ogc@gmail.com>
2015-05-02 18:33:05 -0600

    Make BttHandler implement DataHandler

--- src/main/java/io/github/likcoras/ssbot/backends/BttHandler.java
+++ src/main/java/io/github/likcoras/ssbot/backends/BttHandler.java
@@ -1,15 +1,22 @@
 package io.github.likcoras.ssbot.backends;
 
 import io.github.likcoras.ssbot.ConfigParser;
+import io.github.likcoras.ssbot.data.BttData;
+import io.github.likcoras.ssbot.data.Status;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class BttHandler {
+public class BttHandler implements DataHandler {
+	
+	private static final Pattern SEARCH_PATTERN = Pattern.compile("^batoto\\s+(\\S+.*)");
+	private static final Pattern LINK_PATTERN = Pattern.compile("((http(s)?://)?bato.to/comic/_/(comics/)?\\S+(/)?)");
 	
 	private final String base;
 	
@@ -19,81 +26,112 @@ public class BttHandler {
 		
 	}
 	
-	public String[] getData(String link) {
+	@Override
+	public boolean isHandlerOf(final String query) {
 		
-		try {
-			
-			if (!link.startsWith("http")) {
-				link = "http://" + link;
-			}
-			
-			final Document doc = Jsoup.connect(link).get();
-			final String[] data = new String[5];
-			
-			data[0] = doc.select("h1.ipsType_pagetitle").text();
-			
-			final Elements info = doc.select("td > a");
-			data[1] = info.get(0).text();
-			
-			final Elements tags = doc.select("td > a[href] > span");
-			data[2] = "";
-			for (final Element tag : tags) {
-				
-				data[2] += tag.text() + ", ";
-				
-			}
-			data[2] =
-					data[2].substring(0, data[2].length() - 2).replaceAll(",",
-							"");
-			
-			final Elements stat = doc.select("td");
-			for (final Element a : stat) {
-				if (a.text().equals("Status:")) {
-					data[3] = a.nextElementSibling().text();
-				}
-			}
-			
-			data[4] = link;
-			
-			return data;
-			
-		} catch (final IOException e) {
-			
-			System.out.println("Error: Exception while fetching data!");
-			System.out
-					.println("Please send the following error message to the author:");
-			e.printStackTrace();
-			
-		}
-		
-		return null;
+		return HandlerUtils.checkHandler(query, SEARCH_PATTERN, LINK_PATTERN);
 		
 	}
 	
-	public String[] search(final String search) {
+	@Override
+	public BttData getData(String query) throws NoResultsException {
+		
+		if (!isHandlerOf(query))
+			throw new InvalidHandlerException(query);
 		
 		try {
 			
-			final Document doc = Jsoup.connect(base + search).get();
-			final Elements es = doc.select("strong > a[href]");
+			Matcher searchMatch = SEARCH_PATTERN.matcher(query);
+			if (searchMatch.find())
+				return search(searchMatch.group(1));
 			
-			if (es.isEmpty()) {
-				return null;
-			}
+			Matcher linkMatch = LINK_PATTERN.matcher(query);
+			if (linkMatch.find())
+				return fromLink(linkMatch.group(1));
 			
-			final Element e = es.get(0);
-			return getData(e.attr("href"));
+		} catch (IOException e) {
 			
-		} catch (final IOException e) {
-			
-			System.out.println("Error: Exception while fetching data!");
-			System.out
-					.println("Please send the following error message to the author:");
-			e.printStackTrace();
+			throw new NoResultsException(query, e);
 			
 		}
 		
-		return null;
+		throw new NoResultsException(query);
+		
+	}
+	
+	private BttData search(final String query) throws IOException, NoResultsException {
+		
+		final Document doc = Jsoup.connect(base + query).get();
+		final Elements elements = doc.select("strong > a[href]");
+		
+		if (elements.isEmpty())
+			throw new NoResultsException(query);
+		
+		final Element link = elements.get(0);
+		return fromLink(link.attr("href"));
+		
+	}
+	
+	private BttData fromLink(String link) throws IOException {
+		
+		Document doc = connect(link);
+		BttData btt = new BttData();
+		
+		btt.setLink(link);
+		addTitle(btt, doc);
+		parseTable(btt, doc);
+		
+		return btt;
+		
+	}
+	
+	private Document connect(String link) throws IOException {
+		
+		if (!link.startsWith("http"))
+			link = "http://" + link;
+		
+		return Jsoup.connect(link).get();
+		
+	}
+	
+	private void addTitle(BttData btt, Document doc) {
+		
+		btt.setTitle(doc.select("h1.ipsType_pagetitle").text());
+		
+	}
+	
+	private void parseTable(BttData btt, Document doc) {
+		
+		Elements tableRows = doc.getElementsByTag("table").get(0).getElementsByTag("td[font-weight]");
+		for (Element row : tableRows) {
+			
+			String header = row.text();
+			
+			if (header.equals("Author:"))
+				btt.setAuthor(row.nextElementSibling().text());
+			else if (header.equals("Genres"))
+				addTags(btt, row.nextElementSibling());
+			else if (header.equals("Status:"))
+				addStatus(btt, row.nextElementSibling().text());
+			
+		}
+		
+	}
+	
+	private void addTags(BttData btt, Element element) {
+		
+		Elements tags = element.getElementsByTag("a");
+		for (Element tag : tags)
+			btt.addTag(tag.text());
+		
+	}
+	
+	private void addStatus(BttData btt, String status) {
+		
+		if (status.equals("Complete"))
+			btt.setStatus(Status.COMPLETE);
+		else
+			btt.setStatus(Status.ONGOING);
 		
 	}
 	
