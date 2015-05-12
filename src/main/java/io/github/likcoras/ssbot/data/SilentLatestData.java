c7ddf51a08595c8ab949588f73212ddc7e844feb
likcoras <lck.ogc@gmail.com>
2015-05-11 18:07:17 -0600

    Add SilentLatestData

--- src/main/java/io/github/likcoras/ssbot/data/SilentLatestData.java
+++ src/main/java/io/github/likcoras/ssbot/data/SilentLatestData.java
@@ -1,17 +1,49 @@
 package io.github.likcoras.ssbot.data;
 
+import java.util.Date;
+
 public class SilentLatestData implements SeriesData {
 	
-	private static final String FORMAT = DataUtils.addBold("");
+	private static final String FORMAT = DataUtils.addBold("%bLatest: %b%s (%s days ago) %s");
 	
 	private String title;
 	private String description;
-	private 
+	private Date date;
+	
+	public SilentLatestData() {
+		
+		title = "";
+		description = "";
+		date = null;
+		
+	}
+	
+	public void setTitle(final String name) {
+		
+		title = name;
+		
+	}
+	
+	public void setDescription(final String description) {
+		
+		this.description = description;
+		
+	}
+	
+	public void setDate(Date date) {
+		
+		this.date = date;
+		
+	}
 	
 	@Override
 	public String ircString() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if (title.isEmpty() || description.isEmpty() || date == null)
+			throw new IllegalStateException("Every field must be set in SilentLatestData");
+		
+		return String.format(FORMAT, title, DataUtils.representDateAs(date, DataUtils.DAYS), description);
+		
 	}
 	
 }
