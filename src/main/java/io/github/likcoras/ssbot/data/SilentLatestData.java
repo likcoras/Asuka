df796922cc39f5309b8dc315e06b8751179f96d3
likcoras <lck.ogc@gmail.com>
2015-05-11 18:19:38 -0600

    Add link data value

--- src/main/java/io/github/likcoras/ssbot/data/SilentLatestData.java
+++ src/main/java/io/github/likcoras/ssbot/data/SilentLatestData.java
@@ -4,17 +4,19 @@ import java.util.Date;
 
 public class SilentLatestData implements SeriesData {
 	
-	private static final String FORMAT = DataUtils.addBold("%bLatest: %b%s (%s days ago) %s");
+	private static final String FORMAT = DataUtils.addBold("%bLatest: %b%s %b(%s days ago) %b%s %b%s%b");
 	
 	private String title;
 	private String description;
 	private Date date;
+	private String link;
 	
 	public SilentLatestData() {
 		
 		title = "";
 		description = "";
 		date = null;
+		link = "";
 		
 	}
 	
@@ -36,13 +38,19 @@ public class SilentLatestData implements SeriesData {
 		
 	}
 	
+	public void setLink(String link) {
+		
+		this.link = link;
+		
+	}
+	
 	@Override
 	public String ircString() {
 		
-		if (title.isEmpty() || description.isEmpty() || date == null)
+		if (title.isEmpty() || description.isEmpty() || date == null || link.isEmpty())
 			throw new IllegalStateException("Every field must be set in SilentLatestData");
 		
-		return String.format(FORMAT, title, DataUtils.representDateAs(date, DataUtils.DAYS), description);
+		return String.format(FORMAT, title, DataUtils.representDateAs(date, DataUtils.DAYS), description, link);
 		
 	}
 	
