9e6f04b53bb44856bbdee50faec2a6a6d270dce2
likcoras <lck.ogc@gmail.com>
2015-07-17 16:05:46 -0600

    Update BotResponse

--- src/main/java/io/github/likcoras/asuka/handler/response/BotResponse.java
+++ src/main/java/io/github/likcoras/asuka/handler/response/BotResponse.java
@@ -1,9 +1,9 @@
 package io.github.likcoras.asuka.handler.response;
 
+import io.github.likcoras.asuka.AsukaBot;
+
 public interface BotResponse {
 	
-	public String getMessage();
-	
-	public Target getTarget();
+	public void send(AsukaBot bot);
 	
 }
