870be74594170e96ede707efda9f74c0c0a5a77f
likcoras <lck.ogc@gmail.com>
2015-07-17 17:52:24 -0600

    Slight improvements

--- src/main/java/io/github/likcoras/asuka/handler/response/UptimeResponse.java
+++ src/main/java/io/github/likcoras/asuka/handler/response/UptimeResponse.java
@@ -1,29 +1,28 @@
 package io.github.likcoras.asuka.handler.response;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 import io.github.likcoras.asuka.AsukaBot;
+import io.github.likcoras.asuka.BotUtil;
 
 public class UptimeResponse implements BotResponse {
 	
-	private static final String FORMAT = "I've been up for &b%s&b, since &b%s&b";
+	private static final String FORMAT = BotUtil.addFormat("I've been up for &b%s&b, since &b%s&b");
 	
 	private GenericMessageEvent<PircBotX> event;
 	private String message;
 	
 	public UptimeResponse(GenericMessageEvent<PircBotX> event, long upsince) {
 		this.event = event;
-		Instant since = Instant.ofEpochMilli(upsince);
-		Duration uptime = Duration.ofMillis(event.getTimestamp() - upsince);
-		String upsinceMessage = DateTimeFormatter.ofPattern("MMM dd hh:mm:ss 'UTC'", Locale.US).format(LocalDateTime.ofInstant(since, ZoneId.of("Z")));
-		String uptimeMessage = String.format("%dh %dm %ds", uptime.getSeconds() / 3600, uptime.getSeconds() % 3600 / 60, uptime.getSeconds() % 60);
+		long seconds = upsince / 1000L;
+		long diff = event.getTimestamp() / 1000L - seconds;
+		String upsinceMessage = DateTimeFormatter.ofPattern("MMM dd hh:mm:ss 'UTC'", Locale.US).format(LocalDateTime.ofEpochSecond(seconds, 0, ZoneOffset.UTC));
+		String uptimeMessage = String.format("%dh %dm %ds", diff / 3600, diff % 3600 / 60, diff % 60);
 		message = String.format(FORMAT, uptimeMessage, upsinceMessage);
 	}
 
