217ac229ccb21bc60d1e2665bc24255959ee5fe8
likcoras <lck.ogc@gmail.com>
2015-07-17 16:00:54 -0600

    Tweak slightly

--- src/main/java/io/github/likcoras/asuka/handler/Handler.java
+++ src/main/java/io/github/likcoras/asuka/handler/Handler.java
@@ -1,6 +1,8 @@
 package io.github.likcoras.asuka.handler;
 
+import io.github.likcoras.asuka.AsukaBot;
 import io.github.likcoras.asuka.BotConfig;
+import io.github.likcoras.asuka.exception.ConfigException;
 import io.github.likcoras.asuka.exception.HandlerException;
 import io.github.likcoras.asuka.handler.response.BotResponse;
 import org.pircbotx.PircBotX;
@@ -8,9 +10,9 @@ import org.pircbotx.hooks.Event;
 
 public interface Handler {
 	
-	public void configure(BotConfig config);
+	public void configure(BotConfig config) throws ConfigException;
 	
-	public BotResponse handle(Event<PircBotX> event) throws HandlerException;
+	public BotResponse handle(AsukaBot bot, Event<PircBotX> event) throws HandlerException;
 	
 	public void cleanUp() throws HandlerException;
 	
