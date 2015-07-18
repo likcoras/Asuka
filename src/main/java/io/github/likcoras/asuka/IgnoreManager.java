e0057d63112985e5beb39fd873dc14c84d7873aa
likcoras <lck.ogc@gmail.com>
2015-07-17 18:35:07 -0600

    Wrote up the IgnoreManager

---
+++ src/main/java/io/github/likcoras/asuka/IgnoreManager.java
@@ -0,0 +1,55 @@
+package io.github.likcoras.asuka;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.pircbotx.User;
+import lombok.Cleanup;
+
+public class IgnoreManager {
+	
+	private Set<String> ignored;
+	private Path ignoreFile;
+	
+	public IgnoreManager() {
+		ignored = Collections.synchronizedSet(new HashSet<String>());
+		ignoreFile = Paths.get("ignore.txt");
+	}
+	
+	public void addIgnored(User user) {
+		addIgnored(BotUtil.getId(user));
+	}
+	
+	public void addIgnored(String user) {
+		ignored.add(user);
+	}
+	
+	public void removeIgnored(User user) {
+		removeIgnored(BotUtil.getId(user));
+	}
+	
+	public void removeIgnored(String user) {
+		ignored.remove(user);
+	}
+	
+	public boolean isIgnored(User user) {
+		return ignored.contains(BotUtil.getId(user));
+	}
+	
+	public void writeFile() throws IOException {
+		@Cleanup
+		BufferedWriter write = Files.newBufferedWriter(ignoreFile, StandardOpenOption.CREATE);
+		synchronized(ignored) {
+			for (String user : ignored)
+				write.write(user + "\n");
+		}
+		write.flush();
+	}
+	
+}
