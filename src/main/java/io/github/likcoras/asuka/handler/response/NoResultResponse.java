d280a2f95931b4c19f3019fe7145e58955ff01cc
likcoras <lck.ogc@gmail.com>
2015-07-20 17:08:55 -0600

    Add a NoResultResponse

---
+++ src/main/java/io/github/likcoras/asuka/handler/response/NoResultResponse.java
@@ -0,0 +1,21 @@
+package io.github.likcoras.asuka.handler.response;
+
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+import io.github.likcoras.asuka.AsukaBot;
+
+public class NoResultResponse implements BotResponse {
+	
+	private GenericMessageEvent<PircBotX> event;
+	
+	public NoResultResponse(GenericMessageEvent<PircBotX> event) {
+		this.event = event;
+	}
+	
+	@Override
+	public void send(AsukaBot bot) {
+		event.respond("No results");
+	}
+
+}
