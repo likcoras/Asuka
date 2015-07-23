e8b963e58d7c00cb9200e5b115b3ce24d90a5b6b
likcoras <lck.ogc@gmail.com>
2015-07-22 21:37:29 -0600

    Allow for multi-threaded handling of messages

--- src/main/java/io/github/likcoras/asuka/HandlerManager.java
+++ src/main/java/io/github/likcoras/asuka/HandlerManager.java
@@ -1,19 +1,21 @@
 package io.github.likcoras.asuka;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
 import org.pircbotx.hooks.Event;
 import org.pircbotx.hooks.Listener;
-import org.pircbotx.hooks.types.GenericChannelEvent;
 import org.pircbotx.hooks.types.GenericUserEvent;
 
 import com.google.common.collect.ImmutableList;
 
 import io.github.likcoras.asuka.exception.ConfigException;
-import io.github.likcoras.asuka.exception.HandlerException;
-import io.github.likcoras.asuka.exception.PermissionException;
 import io.github.likcoras.asuka.handler.AuthManageHandler;
 import io.github.likcoras.asuka.handler.BatotoHandler;
 import io.github.likcoras.asuka.handler.Handler;
@@ -23,6 +25,7 @@ import io.github.likcoras.asuka.handler.QuitHandler;
 import io.github.likcoras.asuka.handler.SilentSkyRSSHandler;
 import io.github.likcoras.asuka.handler.SilentSkyXMLHandler;
 import io.github.likcoras.asuka.handler.UptimeHandler;
+import io.github.likcoras.asuka.handler.response.BotResponse;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
 
@@ -31,9 +34,11 @@ public class HandlerManager implements Listener<PircBotX> {
 
 	private AsukaBot bot;
 	private List<Handler> handlers;
+	private ExecutorService executor;
 
 	public HandlerManager(@NonNull AsukaBot bot) {
 		this.bot = bot;
+		executor = Executors.newCachedThreadPool();
 		addHandlers();
 	}
 
@@ -70,19 +75,22 @@ public class HandlerManager implements Listener<PircBotX> {
 	}
 
 	private void handle(Event<PircBotX> event) {
+		List<Future<BotResponse>> futures = new ArrayList<>();
 		for (Handler handler : handlers)
+			futures.add(executor.submit(new HandlerTask(bot, handler, event)));
+		sendOutput(futures);
+	}
+
+	private void sendOutput(List<Future<BotResponse>> futures) {
+		for (Future<BotResponse> future : futures) {
 			try {
-				handler.handle(bot, event).send(bot);
-			} catch (PermissionException e) {
-				log.warn("User " + BotUtil.getId(e.getUser()) + " was denied access for query " + e.getQuery());
-				e.getUser().send().notice("You are not allowed to execute query " + e.getQuery());
-			} catch (HandlerException e) {
-				if (event instanceof GenericChannelEvent || event instanceof GenericUserEvent)
-					event.respond("Error while handling " + event.getClass().getSimpleName() + " with handler "
-							+ handler.getClass().getSimpleName() + ": " + e.getCause().toString());
-				log.warn("Exception caught while handling " + event.getClass().getSimpleName() + " with handler "
-						+ handler.getClass().getSimpleName() + ": ", e);
+				future.get().send(bot);
+			} catch (ExecutionException e) {
+				log.error("Exception caught while handling:", e);
+			} catch (InterruptedException e) {
+				log.error("Interrupted:", e);
 			}
+		}
 	}
 
 }
